2192ce69744b6a3893a873d7a8590170cfef0d6d
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-05-12 11:09:10 -0400

    Update moveZeroesToEnd.js

--- Codewars/5kyu/moveZeroesToEnd.js
+++ Codewars/5kyu/moveZeroesToEnd.js
@@ -1,3 +1,5 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
 function moveZeros(arr) {
     // filter out every zero in the array, then grab them and concat them to the very end
     return arr.filter(x => x !== 0).concat(arr.filter(x => x === 0))
