3910975aa858451846a122b7ed3f994f2cbcefed
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-05-25 14:00:57 -0400

    Create reverseLetter.js

---
+++ Codewars/7kyu/reverseLetter.js
@@ -0,0 +1,30 @@
+// Task
+
+// Given a string str, reverse it omitting all non-alphabetic characters.
+// Example
+
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+// Input/Output
+
+//     [input] string str
+
+// A string consists of lowercase latin letters, digits and symbols.
+
+//     [output] a string
+
+function reverseLetter(str) {
+  // create an empty array to put values into
+  let arr = []
+  // loop through the string, starting from the end
+  for(let i = str.length; i >= 0; i--) {
+    // test if the current character in the loop is a letter
+    if(/[a-z]/.test(str[i])) {
+      // if it is, push the value into our empty array
+      arr.push(str[i])
+    }
+  }
+  // return our reversed array, and join it into a string
+  return arr.join('')
+}
