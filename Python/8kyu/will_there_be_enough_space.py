b8cf81d3440a6adddff4ea5b5e681936095839a9
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-07-01 10:57:35 -0400

    Create will_there_be_enough_space.py

---
+++ Python/8kyu/will_there_be_enough_space.py
@@ -0,0 +1,21 @@
+# Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard
+# his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able
+# to fit all the passengers.
+
+# Task Overview:
+# You have to write a function that accepts three parameters:
+
+# cap is the amount of people the bus can hold excluding the driver.
+# on is the number of people on the bus excluding the driver.
+# wait is the number of people waiting to get on to the bus excluding the driver.
+# If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+def enough(cap, on, wait):
+    # calculate available seats based on how many people are already on
+    seats_left = cap - on
+    # check if there are enough seats for waiting passengers
+    if seats_left > wait:
+        return 0
+    # if not, calculate how many are left over
+    else: 
+        return wait - seats_left
