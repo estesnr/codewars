d970366c9f0b638971d2f254c97e925b520c4d77
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-07-01 20:31:09 -0400

    Create cockroach.py

---
+++ Python/8kyu/cockroach.py
@@ -0,0 +1,15 @@
+# The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the 
+# integer (= floored).
+
+# For example:
+
+# 1.08 --> 30
+# Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer
+
+def cockroach_speed(s):
+    # import the math functions
+    import math
+    # calculate speed
+    speed = (s * 100000) / (60 * 60)
+    # use imported math functions to calculate the answer rounded down
+    return math.floor(speed)
