80c19a9e704a034ec47b5c3fd7ddd1135871eb25
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-07-11 12:54:27 -0400

    Create pay_for_wall.py

---
+++ Python/8kyu/pay_for_wall.py
@@ -0,0 +1,11 @@
+# Don Drumphet lives in a nice neighborhood, but one of his neighbors has started to let his house go. Don Drumphet wants to build a wall between his
+# house and his neighbor’s, and is trying to get the neighborhood association to pay for it. He begins to solicit his neighbors to petition to get the
+# association to build the wall. Unfortunately for Don Drumphet, he cannot read very well, has a very limited attention span, and can only remember two
+# letters from each of his neighbors’ names. As he collects signatures, he insists that his neighbors keep truncating their names until two letters remain,
+# and he can finally read them.
+
+def who_is_paying(name):
+    if len(name) > 2:
+        return [name, name[:2]]
+    else:
+        return [name]
